9569b4ee82a780c08f5694c0853a8e43ff0cd570
Jonas Fleck <jonas@fleckmail.de>
2021-12-29 17:06:30 +0100

    Fix wifi typo

--- wled00/wled.cpp
+++ wled00/wled.cpp
@@ -441,7 +441,7 @@ void WLED::initAP(bool resetAP)
     return;
 
   if (!apSSID[0] || resetAP)
-    strcpy_P(apSSID, PSTR("WLAN-Weihnachtbsaum"));
+    strcpy_P(apSSID, PSTR("WLAN-Weihnachtsbaum"));
   if (resetAP)
     strcpy_P(apPass, PSTR(DEFAULT_AP_PASS));
   DEBUG_PRINT(F("Opening access point "));
